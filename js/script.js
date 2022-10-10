120392665ce1e302b53e1993869d0e8a1b8703ce
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-10-10 15:24:01 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const utenteNome = prompt ('Scrivi il tuo Nome');
+
+const utenteCognome = prompt('Scrivi il tuo Cognome');
+
+const utenteColore = prompt('Scrivi il tuo Colore Preferito') ;
+
+const annoData = ('2022');
+
+// FINE CONST 
+
+document.getElementById('tutto').innerHTML+= utenteNome + ' ' + utenteCognome + ' ' +  utenteColore + ' ' +  annoData;
+
+
